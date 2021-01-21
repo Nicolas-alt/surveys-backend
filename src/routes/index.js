4b3e523438ae949f27a6418e0a273c1de02efa5e
Nicolas-alt <a10100105555@gmail.com>
2021-01-21 18:50:05 -0500

    Add ( App surveys: { question routes } )

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ const app = express();
 app.use(require('./AuthUser'));
 app.use(require('./userRoutes'));
 app.use(require('./surveyRoutes'));
+app.use(require('./surveyQuestion'));
+app.use(require('./surveyAnswer'));
+app.use(require('./userResponse'));
 
 module.exports = app;
