81663d2ef62aa1072f2528afab6581bd28d72094
Nicolas-alt <a10100105555@gmail.com>
2021-01-22 17:04:26 -0500

    FIX CORS:  :)))))))

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // global vars
 require('./src/config/serverConfig');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 mongoose.connect(process.env.URLDB, (err, res) => {
   if (err) throw err;
   console.log('DB ok');
